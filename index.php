7159761cf8d611db29ec03c3e2a759d83caac0a2
root <root@hivetasks.com>
2021-06-08 19:21:09 +0000

    From server

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php /*
+  if(isset($_GET['account'])){
+  if($_GET['account'] == "themarkymark" || $_GET['account'] == "btcvenom"){
+  header("Location: https://www.google.com/search?q=douchebag" );
+  exit();
+  }}
+  if(isset($_GET['login'])){
+  if($_GET['login'] == "themarkymark" ||$_GET['login'] == "btcvenom"){
+  header("Location: https://www.google.com/search?q=douchebag" );
+  exit();
+  }} */
+?><!DOCTYPE html>
+<html lang="EN">
+    <head data-version="<?php echo file_get_contents("version.php") ;?>">
+        <title>HiveTasks</title>
+        <meta charset="UTF-8">
+        <meta name="description" content="Welcome to HiveTasks - Your favorite Tool for the Hive Blockchain.">
+        <meta name="keywords" content="hive,tool,dashboard,statistics,blockchain">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="/favicon.png" sizes="32x32">
+        <link rel="icon" type="image/png" href="/favicon.png" sizes="96x96">
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180">
+        <link type="text/css" rel="stylesheet" href="/res/css/jquery-ui-1.12.1-base/jquery-ui.min.css">
+        <link type="text/css" rel="stylesheet" href="/res/css/fonts.css">
+        <link type="text/css" rel="stylesheet" href="/res/css/main.min.css?v=2396">
+        <script src="/res/js/sw.libs.min.js" defer></script>
+        <script src="/res/js/sw.out.min.js" defer></script>
+        <script src="/res/js/pr.js" async></script>
+    </head>
+    <body class="responsive">
+        <section id="pageContent" data-module="<?php
+        echo isset($_GET['data-module']) ? $_GET['data-module'] : "main"
+        ?>" data-account="<?php
+                 echo isset($_GET['account']) ? $_GET['account'] : "{account}"
+                 ?>" data-theme="" data-cfg="{cfg}" data-login="{login}" data-block_num="<?php
+                 echo isset($_GET['block_num']) ? $_GET['block_num'] : "{block_num}"
+                 ?>" data-trx_id="<?php
+                 echo isset($_GET['trx_id']) ? $_GET['trx_id'] : "{trx_id}"
+                 ?>" data-trx_index="<?php
+                 echo isset($_GET['trx_index']) ? $_GET['trx_index'] : "{trx_index}"
+                 ?>" data-virtual_op="<?php
+                 echo isset($_GET['virtual_op']) ? $_GET['virtual_op'] : "{virtual_op}"
+                 ?>" data-author="<?php
+                 echo isset($_GET['author']) ? $_GET['author'] : "{author}"
+                 ?>" data-permlink="<?php
+                 echo isset($_GET['permlink']) ? $_GET['permlink'] : "{permlink}"
+                 ?>"></section>
+        <noscript id="pageNoScript">
+        <h3>
+            Please enable JavaScript to enter HiveTasks...
+            <br><br>
+            <a href="#">Try again</a>
+        </h3>
+        </noscript>
+
+    </body>
+</html>
