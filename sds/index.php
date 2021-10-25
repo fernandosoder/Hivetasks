ca77172ffbdfb6e6df7b611c2fe45b559fb1479c
Fernando Soder <fhsoder@gmail.com>
2021-10-25 15:27:28 -0300

    Testing stuff

--- sds/index.php
+++ sds/index.php
@@ -1,1 +1,24 @@
-<?php // silence for now
+<?php 
+header("Access-Control-Allow-Origin: *");
+
+//print_r($_GET);
+
+switch($_GET['var1']){
+	case 'ticker_api' :
+		switch($_GET['var2']){
+			case 'getTicker' :
+					switch($_GET['var3']){
+						case 'rates' :
+							header("Content-Type: application/json");
+							include "rates.json";
+							break;
+					}
+				break;
+		}
+		break;
+	case '' :
+		break;
+	case '' :
+		break;
+	
+}
